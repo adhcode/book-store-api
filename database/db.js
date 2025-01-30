6fd52234fb35f72bbfa58b442bea196714f7b294
ADHHHH <adh.devv@gmail.com>
2025-01-30 14:35:14 +0100

    db

--- database/db.js
+++ database/db.js
@@ -3,9 +3,7 @@ const dns = require("dns");
 const connectToDB = async () => {
   try {
     dns.setServers(["8.8.8.8", "8.8.4.4"]);
-    await mongoose.connect(
-      "mongodb+srv://adekunledh:GoodGod11@cluster0.pz0qq.mongodb.net/"
-    );
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("Connected to MongoDB");
   } catch (error) {
     console.error("MongoDB connection error:", error);
